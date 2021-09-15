47ab4450a5a5af65d36fd063c2973e3fd58a553f
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-15 11:36:21 -0300

    Add Pokemon Routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,27 @@
 import { Router } from 'express';
+import { AddPokemonControler } from './controller/AddPokemonUseCase/AddPokemonController';
+import { DetailsPokemonController } from './controller/DetailsPokemonUseCase/DetailsPokemonController';
+import { ExcludePokemonController } from './controller/ExcludePokemonUseCase/ExcludePokemonController';
+import { FillterPokemonsController } from './controller/FillterPokemonsUseCase/FillterPokemonsController';
+import { FindCapturedPokemonsController } from './controller/FindCapturedPokemonsUseCase/FindCapturedPokemonsController';
 import TrainerController from './controller/SignUp/SignUp';
+
+const addPokemonControler = new AddPokemonControler();
+const findCapturedPokemonsController = new FindCapturedPokemonsController();
+const detailsPokemonController = new DetailsPokemonController();
+const excludePokemonController = new ExcludePokemonController();
+const fillterPokemonsController = new FillterPokemonsController();
 
 const routes = Router();
 
 routes.get('/', (request, response) => response.json());
-routes.post('/signup', TrainerController.store )
+routes.post('/signup', TrainerController.store);
+
+routes
+  .post('/pokemons', addPokemonControler.handle)
+  .get('/pokemons', findCapturedPokemonsController.handle)
+  .get('/pokemons/filter', fillterPokemonsController.handle)
+  .get('/pokemons/:poke_id', detailsPokemonController.handle)
+  .delete('/pokemons/:poke_id', excludePokemonController.handle);
+
 export { routes };
