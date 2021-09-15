8fd819425bfa4f4297a847b53e34c2d0d45f062b
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-15 06:02:47 -0300

    Add Pokemon Entity

---
+++ backend/src/entities/Pokemon.ts
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+import {
+  Entity, Column, PrimaryColumn, CreateDateColumn,
+} from 'typeorm';
+
+@Entity('pokemons')
+class Pokemon {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  trainer_id: string;
+
+  @Column()
+  name: string;
+
+  @Column('text', { array: true })
+  types: string[];
+
+  @Column()
+  image: string;
+
+  @Column()
+  weight: number;
+
+  @Column()
+  height: number;
+
+  @Column()
+  attack: number;
+
+  @Column()
+  defense: number;
+
+  @Column('text', { array: true })
+  abilities: string[];
+
+  @CreateDateColumn()
+  created_at: Date;
+}
+export { Pokemon };
