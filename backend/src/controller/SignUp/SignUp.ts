0e6de44c4cd43ebe15636cf76f2405218cb628e2
Moizes Ferreira dos Santos <moizesferreira66@hotmail.com>
2021-09-15 18:33:34 -0300

    commit

--- backend/src/controller/SignUp/SignUp.ts
+++ backend/src/controller/SignUp/SignUp.ts
@@ -4,15 +4,24 @@ import Trainer from "../../entities/Trainer";
 
 
 // criação de treinador pokemon
-class TrainerController{
-    async store(req: Request,res: Response){
-       const repository = getRepository(Trainer);
-       const { nick_name, email, password } = req.body;
+class SignUp {
+    async store(req: Request, res: Response) {
+        const repository = getRepository(Trainer);
+        const { nick_name, email, password } = req.body;
 
-       const trainerExist = await repository.findOne({ where: { email }});
-        if(trainerExist){
-            return res.sendStatus(409).send('Informações de dados ja usadas')
+        const trainerExist = await repository.findOne({ where: { email } });
+
+        // limitando para nao repetir email
+        if (trainerExist) {
+            return res.sendStatus(409);
         }
+
+        const trainer = repository.create({ nick_name, email, password });
+
+        //salvando os dados recebidos no banco de dados
+        await repository.save(trainer);
+
+        return res.json(trainer);
     }
 }
-export default new TrainerController();
+export default new SignUp();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,9 @@ import { routes } from './routes';
 import './database/connections'
 const app = express();
 
+app.use(express.json())
 app.use(routes);
+
 
 // eslint-disable-next-line no-console
 app.listen(3003, () => console.log('Runing!'));
