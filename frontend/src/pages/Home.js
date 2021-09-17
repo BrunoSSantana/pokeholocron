b11608c0d355efa1a3fd11280ae902bcc4d947e4
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-17 08:31:16 -0300

    Order pokemons

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -34,6 +34,16 @@ export default function Home() {
 
   }
 
+  setPokemons(pokemons.sort(function (a,b) {
+    if (a.id > b.id) {
+      return 1
+    }
+    if (a.id < b.id) {
+      return -1
+    }
+    return 0
+  }))
+
   function filterById(pokemonId) {
     const newPokemons = []
 
