0b534d1e7c6580dc3c04a79c407c3576ee2c66bd
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-18 16:50:08 -0300

    Refactor

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -38,9 +38,9 @@ export default function Home() {
   //   filterByName()
 
   // }, [name])
-
   
-  let newPokemons = useMemo(()=> {
+
+  let newPokemons = useMemo(() => {
     const lowerName = name.toLowerCase()
 
     return pokemons.filter(pokemon => {
@@ -48,107 +48,120 @@ export default function Home() {
     })
   }, [name, pokemons])
 
+
+  // newPokemons = useMemo(() => {
+
+  //   return pokemons.filter(pokemon => {
+  //     return pokemon.id === Number(pokeId)
+  //   })
+
+
+  // }, [pokeId, pokemons])
+
+
+
+
   // useEffect(() => {
   //   function filterByType() {
 
-  //     const newPokemons = []
+  // const filtraPorTipo = pokemon => {
+  //   // console.log(pokemon.name);
+  //   return pokemon.types.forEach(slot => {
+  //     console.log(slot.type.name);
+  //     if (slot.type.name === typePokemon) {
+  //       return true
+  //     }
+  //   })
+    
+  // }
 
-  //     pokemons.forEach(pokemon => {
-  //       pokemon.types.forEach(slot => {
-  //         if (slot.type.name === typePokemon) {
-  //           newPokemons.push(pokemon)
-  //         }
-  //       })
-  //     })
+  // let newPokemons = pokemons.filter(filtraPorTipo)
 
-  //     setPokemons(newPokemons);
-  //   }
+  // setPokemons(newPokemons);
+// }
 
-  //   filterByType()
+//   filterByType()
 
-  // }, [typePokemon])
+// }, [typePokemon])
 
-  // useEffect(() => {
-  //   function filterById() {
-  //     const newPokemons = []
-
-  newPokemons = useMemo(() =>{
-    return pokemons.filter(pokemon => {
-      return pokemon.id === pokeId
-    })
-
-  },[pokeId, pokemons]) 
-
-  //     setPokemons(newPokemons);
-  //   }
-  //   filterById()
-
-  // }, [pokeId])
+// useEffect(() => {
+//   function filterById() {
+//     const newPokemons = []
 
 
-  return (
-    <div className={style.pokedex_container}>
 
-      <div className={style.header}>Pokemons 1º Generation</div>
-      <Link to='/pokedex'>Pokedex</Link>
 
-      <div className={style.inputs}>
-        <div className="input">
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" value={name} onChange={(e) => { setName(e.target.value) }} />
-        </div>
 
-        <div className={style.input}>
-          <label htmlFor="type">Type:</label>
-          <select name="type" id="type" className={style.type} value={typePokemon} onChange={(e) => { setTypePokemon(e.target.value) }}>
-            {types.map(type => {
-              return (<option key={type}>{type}</option>)
-            })}
-          </select>
-        </div>
+//     setPokemons(newPokemons);
+//   }
+//   filterById()
 
-        <div className="input">
-          <label htmlFor="">Id: </label>
-          <input type="text" id="pokeId" value={pokeId} onChange={(e) => { setPokeId(e.target.value) }} />
-        </div>
+// }, [pokeId])
+
+
+return (
+  <div className={style.pokedex_container}>
+
+    <div className={style.header}>Pokemons 1º Generation</div>
+    <Link to='/pokedex'>Pokedex</Link>
+
+    <div className={style.inputs}>
+      <div className="input">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" value={name} onChange={(e) => { setName(e.target.value) }} />
       </div>
 
-      <div className={style.card_container}>
-
-        {newPokemons.map((val) => {
-          const types = []
-          const abilities = []
-
-          val.types.forEach(slot => {
-            types.push(slot.type.name)
-          })
-
-          val.abilities.forEach(slot => {
-            abilities.push(slot.ability.name)
-          })
-
-          return (
-            <div key={val.name}>
-              <CardComponent
-                pokemon={{
-                  name: val.name,
-                  poke_id: val.id,
-                  types,
-                  img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${val.id}.svg`,
-                  abilities,
-                  weight: val.weight,
-                  attack: val.stats[1].base_stat,
-                  defense: val.stats[2].base_stat,
-                  height: val.height
-                }}
-              />
-            </div>
-          )
-        })}
-
+      <div className={style.input}>
+        <label htmlFor="type">Type:</label>
+        <select name="type" id="type" className={style.type} value={typePokemon} onChange={(e) => { setTypePokemon(e.target.value) }}>
+          {types.map(type => {
+            return (<option key={type}>{type}</option>)
+          })}
+        </select>
       </div>
 
+      <div className="input">
+        <label htmlFor="">Id: </label>
+        <input type="text" id="pokeId" value={pokeId} onChange={(e) => { setPokeId(e.target.value) }} />
+      </div>
+    </div>
+
+    <div className={style.card_container}>
+
+      {newPokemons.map((val) => {
+        const types = []
+        const abilities = []
+
+        val.types.forEach(slot => {
+          types.push(slot.type.name)
+        })
+
+        val.abilities.forEach(slot => {
+          abilities.push(slot.ability.name)
+        })
+
+        return (
+          <div key={val.name}>
+            <CardComponent
+              pokemon={{
+                name: val.name,
+                poke_id: val.id,
+                types,
+                img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${val.id}.svg`,
+                abilities,
+                weight: val.weight,
+                attack: val.stats[1].base_stat,
+                defense: val.stats[2].base_stat,
+                height: val.height
+              }}
+            />
+          </div>
+        )
+      })}
 
     </div>
-  )
+
+
+  </div>
+)
 }
